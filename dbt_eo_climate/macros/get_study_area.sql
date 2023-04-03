7ee5c0786c988eeefc8bd6aa5651a38fb513dc10
mrbertrand@gmail.com <mrbertrand@gmail.com>
2023-04-03 14:13:22 +0000

    Add schema models

--- dbt_eo_climate/macros/get_study_area.sql
+++ dbt_eo_climate/macros/get_study_area.sql
@@ -9,11 +9,8 @@
         when 'pusch_ridge_north' then 'Pusch Ridge (North)'
         when 'saguaro_np_east' then 'Saguaro National Park (East)'
         when 'saguaro_np_west' then 'Saguaro National Park (West)'
-<<<<<<< HEAD
         when 'mt_wrightson_south' then 'Mt. Wrightson (South)'
-=======
         when 'mt_wrightson_south' then 'My Wrightson (South)'
->>>>>>> e8eaae7f6819cb415f82863ceb154f37b6a9bd4b
     end
 
 {%- endmacro %}
